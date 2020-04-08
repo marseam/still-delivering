eb000b578860afda2125d510906af020259a431a
marseam <mmcgee121@gmail.com>
2020-04-08 15:29:12 +0100

    Updates

---
+++ precache-manifest.69c707233ea6a7a3a717d1ba3c521bb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663c55455dd96be2464d8d88f7ea5b06",
+    "url": "/marseam/still-delivering/index.html"
+  },
+  {
+    "revision": "b5802ad877335dfecefa",
+    "url": "/marseam/still-delivering/static/css/2.1c5b230e.chunk.css"
+  },
+  {
+    "revision": "b5802ad877335dfecefa",
+    "url": "/marseam/still-delivering/static/js/2.2c27a572.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/marseam/still-delivering/static/js/2.2c27a572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cca1380b9acc19a8ede",
+    "url": "/marseam/still-delivering/static/js/main.70c5754a.chunk.js"
+  },
+  {
+    "revision": "0e99f18ef48a01033cf6",
+    "url": "/marseam/still-delivering/static/js/runtime-main.4604c802.js"
+  }
+]);
